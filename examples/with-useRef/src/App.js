2b9144226950760bd9bb755f0d4de70605475e4a
Ruben <ruben@leija.io>
2019-04-09 09:24:36 -0400

    Create input field animation

--- examples/with-useRef/src/App.js
+++ examples/with-useRef/src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 
 import styles from './App.module.css';
 
 const App = () => {
 
+  const inputGroupRef = useRef();
+  const inputRef = useRef();
+
+  useLayoutEffect(() => {
+    const { current  } = inputRef;
+
+    current.addEventListener('focus', () =>
+      inputGroupRef.current.classList.add(styles.active))
+
+    current.addEventListener('blur', () =>
+      inputGroupRef.current.classList.remove(styles.active))
+
+  }, [])
+
   return (
     <div className={styles.container}>
-      <div className={styles.inputGroup}>
-        <label>Your name</label>
-        <input placeholder="Your name" />
+      <div ref={inputGroupRef} className={styles.inputGroup}>
+        <label className={styles.label}>
+          Your name
+        </label>
+        <input
+          ref={inputRef}
+          className={styles.input}
+          type="text"
+          autoComplete="off"/>
+        <div className={styles.border}/>
       </div>
     </div>
   )
