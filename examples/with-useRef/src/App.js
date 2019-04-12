cf0447a550a38871c93b79c1fd538301bd953f11
Ruben <ruben@leija.io>
2019-04-12 08:07:31 -0400

    Fix: label stays hovered if input has value

--- examples/with-useRef/src/App.js
+++ examples/with-useRef/src/App.js
@@ -4,19 +4,37 @@ import styles from './App.module.css';
 
 const App = () => {
 
+  // Step 1: Create ref object for iput group container.
+  // and input field
   const inputGroupRef = useRef();
   const inputRef = useRef();
 
+  // Step 2: Add useLayoutEffect and pass an empty array
+  // for it to execute only on componentDidMount lifecycle
   useLayoutEffect(() => {
     const { current  } = inputRef;
 
-    current.addEventListener('focus', () =>
-      inputGroupRef.current.classList.add(styles.active))
+    // Step 3: Create functions to handle on focus and on blur
+    // event listeners.
+    const handleFocus = () => inputGroupRef.current.classList.add(styles.active);
+    const handleBlur = () => {
+      if (!current.value.length) {
+        inputGroupRef.current.classList.remove(styles.active)
+      }
+    };
 
-    current.addEventListener('blur', () =>
-      inputGroupRef.current.classList.remove(styles.active))
+    // Step 4: Add event listeners for focus and blur.
+    current.addEventListener('focus', handleFocus);
+    current.addEventListener('blur', handleBlur);
 
+    // Handle removal of event listeners when
+    // component is unmounting.
+    return () => {
+      current.removeEventListener('focus', handleFocus);
+      current.removeEventListener('blur', handleBlur);
+    }
   }, [])
+
 
   return (
     <div className={styles.container}>
