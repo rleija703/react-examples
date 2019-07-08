865c22bdd0a342c50922ef1522b8c64f53fce06f
Ruben <ruben@leija.io>
2019-07-08 06:58:22 -0400

    Add with-typescript

--- examples/with-router-lazy-loading/src/App.js
+++ examples/with-router-lazy-loading/src/App.js
@@ -18,7 +18,7 @@ const App = () => {
         <div className="col-sm-5">
         <React.Suspense fallback={<span>Loading...</span>}>
           <Router>
-            <ol className="breadcrumb">
+            <ol>
               <NavLink
                 to="/"
                 className="breadcrumb-item"
---
+++ examples/with-typescript/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>TypeScript</h1>,
+  document.getElementById("example")
+);
