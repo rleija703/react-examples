3cc40992b8e50fd18d97119f4114bd128d0d9863
Ruben <ruben@leija.io>
2019-07-19 06:40:25 -0400

    Update /useref-with-typescript/App.tsx file

--- useref-with-typescript/src/App.tsx
+++ useref-with-typescript/src/App.tsx
@@ -3,10 +3,10 @@ import React, { useRef, useLayoutEffect } from 'react';
 const App = () => {
   const h1Ref = useRef<HTMLHeadingElement>(null);
 
-  console.log(h1Ref)
+  console.log(h1Ref) // { current: null }
 
   useLayoutEffect(() => {
-    console.log(h1Ref);
+    console.log(h1Ref); // { current: <h1_object> }
   })
 
   return (
