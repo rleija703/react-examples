eed9458d5bdccb96651b19fd971f45f6c2ae7554
Ruben <ruben@leija.io>
2019-08-17 08:56:43 -0400

    Add react

---
+++ with-pure-component/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Greeting extends React.Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Greeting - should component update lifecycle');
+
+    return false;
+  }
+
+  render() {
+    console.log('Greeting - render lifecycle')
+    return <h1>Hi there, my name is Ruben!</h1>
+  }
+}
+
+class App extends React.Component {
+
+  state = {
+    greeted: false,
+  };
+
+  componentDidMount() {
+    this.setState({greeted: true});
+  }
+
+  render() {
+    console.log('App - render lifecycle')
+    return <Greeting />
+  }
+}
+
+export default App;
