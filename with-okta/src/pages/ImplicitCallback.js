94685574fca7aac81ca38b1b4aa47aa12b45afbe
Ruben <ruben@leija.io>
2019-09-02 07:43:54 -0400

    Remove commented code

--- with-okta/src/pages/ImplicitCallback.js
+++ with-okta/src/pages/ImplicitCallback.js
@@ -21,34 +21,3 @@ export default withAuth(class ImplicitCallback extends Component {
     return <h1>The Okta callback</h1>
   }
 });
-
-// export default withAuth(class ImplicitCallback extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       authenticated: null,
-//       error: null
-//     };
-//   }
-
-//   componentDidMount() {
-//     this.props.auth.handleAuthentication()
-//     .then(() => this.setState({ authenticated: true }))
-//     .catch(err => this.setState({ authenticated: false, error: err.toString() }));
-//   }
-
-//   render() {
-//     if (this.state.authenticated === null) {
-//       return null;
-//     }
-
-//     const referrerKey = 'secureRouterReferrerPath';
-//     const location = JSON.parse(localStorage.getItem(referrerKey) || '{ "pathname": "/" }');
-//     localStorage.removeItem(referrerKey);
-
-//     return this.state.authenticated ?
-//       <Redirect to={location}/> :
-//       <p>{this.state.error}</p>;
-//   }
-// });
